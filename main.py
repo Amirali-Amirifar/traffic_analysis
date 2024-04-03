0d399ab62833c2a43ac0f1fad0a1cdda914c4b90
amirali <amirali@kali>
2024-04-03 22:17:42 +0330

    add requirements

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import pandas as pd
+import numpy as np
+import matplotlib as plt
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
